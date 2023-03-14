be689e7e3ce895bcf956ca4e2d5deea2b801949e
Kacper Ledwosiński <kl@airotec.pl>
2023-03-14 14:05:05 +0100

    workflow - average time of detection

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # figure-workflow
+
+1. copy figures repo
+```
+git clone git@github.com:kapipapi/figures.git
+```
+
+2. copy detector repo
+```
+git clone git@github.com:kapipapi/figure-detector.git detector
+```
---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+
+from detector.detector import Detector
+from detector.letter_model import LetterModel
+from figures.generate import get_figure_sample, load_classes
+from figures.util import load_map
+
+if __name__ == "__main__":
+    map_img = load_map("./assets/suasorto.tif")
+
+    img, cxywh = get_figure_sample(load_classes(), map_img)
+
+    d = Detector("../yolov5", "../yolov5/runs/train/exp5/weights/best.pt")
+    lm = LetterModel("../figure-detector/model_weights.pth")
+
+    time_avg = 0
+    for i in range(100):
+        start = time.time()
+        results = d.detect_with_letters(img, lm)
+        for r in results:
+            print()
+            print("number of figures detected: ", len(results))
+            print("box:", r["box"])
+            print("conf:", r["conf"])
+            print("cls:", r["cls"])
+            print("label:", r["label"])
+            if "letter" in r:
+                print("letter:", r["letter"])
+
+        time_avg += (time.time() - start)
+
+    print("time average: ", time_avg / 100)
